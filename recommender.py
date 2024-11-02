e4103509610a1eb8c9851e5f7b6d0914299cc42c
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-11-02 18:03:38 +0000

    Give default value for class attributes

--- recommender.py
+++ recommender.py
@@ -1,7 +1,14 @@
+from dataclasses import dataclass
 import pandas as pd 
 
-class Recommender():
+@dataclass
+class SearchResult:
+    genres: list = None 
+    year: int = None
+    rating: int = None
+
+class Recommender:
     def __innit__(self):
-        data = pd.read_csv('imdb_top_1000.csv')
+        self.data = pd.read_csv('imdb_top_1000.csv')
 
     
