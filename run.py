155e3ecd22bfe6255e7cc8b943eec954f06e9bbb
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-11-02 13:36:29 +0000

    Add question for rating and validation

--- run.py
+++ run.py
@@ -13,6 +13,18 @@ def validate_year(year):
     except ValueError:
         return False
     
+def validate_rating(rating):
+    """Validate if the rating is between 0 and 100 or not provided"""
+    try:
+        if rating == "":
+            return True
+        rating = int(rating)
+        if rating < 0 or rating > 100:
+            return False
+        return True
+    except ValueError:
+        return False
+    
 
 def main():
     
@@ -60,7 +72,7 @@ def main():
             {
                 "type": "input",
                 "message": "Enter a year between 1920 and 2020",
-                "instruction": "(Press enter to confirm)",
+                "instruction": "(Type your answer and press enter to confirm)",
                 "validate": lambda result: validate_year(result),
                 "invalid_message": "Year must be between 1920 and 2020",
             }
@@ -69,7 +81,20 @@ def main():
 
     chosen_year = year_questions[0]
 
-    
+    rating_questions = prompt(
+        [
+            {
+                "type": "input",
+                "message": "Enter a rating between 0 and 100",
+                "instruction": "(Type your answer and press enter to confirm)",
+                "validate": lambda result: validate_rating(result),
+                "invalid_message": "Rating must be between 0 and 100",
+            }
+        ]
+    )
+
+    chosen_rating = rating_questions[0]
+    print(chosen_genres, chosen_year, chosen_rating)
 
 main()
 
