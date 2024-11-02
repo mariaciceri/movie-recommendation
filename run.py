8dd37f5b01a9f923e17b766e143c9f56f208daca
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-11-02 13:30:50 +0000

    Add movie year question and validation

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 from InquirerPy import prompt
 
 
+def validate_year(year):
+    """Validate if the year is between 1920 and 2020 or not provided"""
+    try:
+        if year == "":
+            return True
+        year = int(year)
+        if year < 1920 or year > 2020:
+            return False
+        return True
+    except ValueError:
+        return False
+    
+
 def main():
     
-    movies_categories = [
+    movies_genre = [
         "Film-Noir",
         "Drama",
         "Mystery",
@@ -33,10 +46,30 @@ def main():
             {
                 "type": "checkbox",
                 "message": "Choose a genre",
-                "choices": movies_categories,
+                "choices": movies_genre,
                 "instruction": "(Use arrow up/down to see more genre options. Use space to select, enter to confirm)",
             }
         ]
     )
 
+    chosen_genres = genre_questions[0]
+
+
+    year_questions = prompt(
+        [
+            {
+                "type": "input",
+                "message": "Enter a year between 1920 and 2020",
+                "instruction": "(Press enter to confirm)",
+                "validate": lambda result: validate_year(result),
+                "invalid_message": "Year must be between 1920 and 2020",
+            }
+        ]
+    )
+
+    chosen_year = year_questions[0]
+
+    
+
+main()
 
