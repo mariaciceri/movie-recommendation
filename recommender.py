637ea6381bd46100258d040c93b4f26cd1ead94a
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-11-03 09:01:49 +0000

    Create func to return the info about chosen movie

--- recommender.py
+++ recommender.py
@@ -11,22 +11,27 @@ class Recommender:
     def __init__(self):
         self.data = pd.read_csv('imdb_top_1000.csv')
 
-    def search_movies(self, search_result):
+    def search_movies_by_genre(self, search_result):
         movies = self.data
+
         if search_result.genres:
             search_result.genres.sort()
-            genre_pattern = ', '.join(search_result.genres)
-            movies = movies.loc[movies["Genre"].str.contains(genre_pattern, case=False, na=False)]
-            movies_titles = movies["Series_Title"].tolist()
-            print(f"Movies with genres {search_result.genres}: {movies_titles if movies_titles else 'No movies found'}")
+            genre_pattern = ', '.join(search_result.genres) #Converting list to string
+            movies_found = movies.loc[movies["Genre"].str.contains(genre_pattern, case=False, na=False)]
+            movies_titles = movies_found["Series_Title"].tolist() #Converting series to list
+            return movies_titles if movies_titles else 'No movies found'
 
+
+    def search_movies_by_year(self, search_result):
         if search_result.year:
             movies = movies.loc[movies["Year"] == search_result.year]
+
+
+    def search_movies_by_rating(self, search_result):
         if search_result.rating:
             movies = movies.loc[movies["IMDB_Rating"] > search_result.rating]
-        return movies
-    
-
-test = SearchResult(genres=["War", "Comedy", "Fantasy"])
-recommender = Recommender()
-print(recommender.search_movies(test))
+        
+    def filtered_data(self, movie):
+        filtered_data = self.data.query(f'Series_Title == "{movie}"')
+        columns_to_show = [self.data.columns[i] for i in [1, 2, 4, 5, 8, 9, 10]]
+        return filtered_data[columns_to_show]
